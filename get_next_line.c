6272c5e2206fbf18ff04a32df811ecf29efbe79c
Salah-Eddine Amsaafi <samsaafi@c3r6p8.1337.ma>
2023-12-10 13:02:09 +0100

    get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,131 @@
+#include "get_next_line.h"
+
+char    *ft_join_and_free(char *stash, char *buffer)
+{
+    char *ptr;
+
+    ptr = ft_strjoin(stash, buffer);
+    free(stash);
+    return (ptr);
+}
+
+char    *read_file(int fd, char *stash)
+{
+    char    *buffer;
+    ssize_t bytes_read;
+
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    if (!buffer)
+        return (NULL);
+    bytes_read = 1;
+    while (bytes_read > 0)
+    {
+        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_read == -1 || bytes_read == 0)
+        {
+            free (buffer);
+            return (NULL);
+        }
+        stash = ft_join_and_free(stash, buffer);
+        if (ft_strchr(stash, '\n'))
+            break;
+    }
+    free (buffer);
+    return (stash);
+}
+
+
+char    *get_line(char *stash)
+{
+    char    *line;
+    int i;
+
+    i = 0;
+    while (stash[i] && stash[i] != '\n')
+        i++;
+
+    line = ft_calloc(i + 1, sizeof(char));
+    if (!line)
+        return (NULL);
+    
+    i = 0;
+    while (stash[i] && stash[i] != '\n')
+    {
+        line[i] = stash[i];
+        i++;
+    }
+    if (stash[i] == '\n')
+    {
+        line[i] = '\n';
+        line[i + 1] = '\0';
+    }
+    return (line);
+}
+
+char    *ft_reset_stash(char *stash)
+{
+    char *new_stash;
+    int i, j;
+
+    i = 0;
+    while (stash[i] && stash[i] != '\n')
+        i++;
+    
+    if (!stash[i])
+    {
+        free(stash);
+        return (NULL);
+    }
+
+    new_stash = ft_strdup(stash + i + 1);
+    free(stash);
+    return (new_stash);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *stash;
+    char    *line;
+
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    if (!stash)
+        stash = ft_calloc(1, sizeof(char));
+    if (!ft_strchr(stash, '\n'))
+        stash = read_file(fd, stash);
+    if (!stash)
+    {
+        free (stash);
+        return (NULL);
+    }
+    line = get_line(stash);
+    stash = ft_reset_stash(stash);
+    return (line);
+}
+
+int main ()
+{
+    char    *result;
+    int fd;
+
+    fd = open("lines.txt", O_RDONLY);
+    if (fd == -1) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    while ((result = get_next_line(fd)) != NULL)
+    {
+        printf("%s", result);
+        free(result);
+    }
+    // result = get_next_line(fd);
+    // printf("%s", result);
+    // free(result);
+    // result = get_next_line(fd);
+    // printf("%s", result);
+    // free(result);
+    close(fd);
+    return 0;
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,100 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_memset(void *ptr, int value, size_t num)
+{
+	unsigned char	*byte_ptr;
+	size_t			i;
+
+	byte_ptr = (unsigned char *)ptr;
+	i = 0;
+	while (i < num)
+	{
+		byte_ptr[i] = (unsigned char)value;
+		i++;
+	}
+	return (ptr);
+}
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*ptr;
+
+	if (num != 0 && size > SIZE_MAX / num)
+		return (NULL);
+	ptr = malloc(num * size);
+	if (ptr)
+	{
+		ft_memset(ptr, '\0', num * size);
+		return (ptr);
+	}
+	return (NULL);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while ((char)c != *s)
+	{
+		if (!*s)
+			return (0);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	tlen;
+	char	*ptr;
+    size_t	i;
+	size_t	j;
+
+	tlen = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	ptr = malloc(tlen);
+	if (!ptr)
+		return (NULL);
+    i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(s);
+	dup = (char *)malloc(len * sizeof(char) + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
